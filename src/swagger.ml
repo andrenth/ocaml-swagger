566caa62a97b3154d463ae37d33739f56de10882
Andre Nathan <andre@digirati.com.br>
2017-12-08 17:39:37 -0200

    First commit

---
+++ src/swagger.ml
@@ -0,0 +1,8 @@
+open Printf
+
+let () =
+  let ic = open_in "swagger.json" in
+  let s = really_input_string ic (in_channel_length ic) in
+  let swagger = Swagger_j.t_of_string s in
+  let s = Swagger_j.string_of_t swagger in
+  printf "%b\n%!" (swagger = Swagger_j.t_of_string s)
