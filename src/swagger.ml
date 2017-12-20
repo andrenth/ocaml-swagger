47d4c4643e7ec362701ed9fb91f214b4a763d34b
Andre Nathan <andrenth@gmail.com>
2017-12-20 15:16:00 -0200

    Rename types, add .merlin

--- src/swagger.ml
+++ src/swagger.ml
@@ -3,6 +3,6 @@ open Printf
 let () =
   let ic = open_in "swagger.json" in
   let s = really_input_string ic (in_channel_length ic) in
-  let swagger = Swagger_j.t_of_string s in
-  let s = Swagger_j.string_of_t swagger in
-  printf "%b\n%!" (swagger = Swagger_j.t_of_string s)
+  let swagger = Swagger_j.swagger_of_string s in
+  let s = Swagger_j.string_of_swagger swagger in
+  printf "%b\n%!" (swagger = Swagger_j.swagger_of_string s)
