f277965e83b2758ec230c558e9abc310ae87792b
Andre Nathan <andre@digirati.com.br>
2018-03-09 16:34:08 -0300

    Rename module

--- src/swagger.ml
+++ src/swagger.ml
@@ -5,9 +5,9 @@ let () =
   let s = really_input_string ic (in_channel_length ic) in
   let swagger = Swagger_j.swagger_of_string s in
   let signature =
-    Sig_gen.of_swagger
+    Gen.of_swagger
       ~path_base:"/api/"
       ~definition_base:"io.k8s."
       ~reference_base:"#/definitions/io.k8s."
       swagger in
-  printf "%s\n%!" (signature |> Sig_gen.to_string)
+  printf "%s\n%!" (signature |> Gen.to_string)
