b0ded9a2810618ad1f12dde096ae4b7cf0c136d2
Andre Nathan <andre@digirati.com.br>
2018-03-14 18:18:08 -0300

    Simple refactor

--- src/swagger.ml
+++ src/swagger.ml
@@ -4,10 +4,10 @@ let () =
   let ic = open_in "src/swagger.json" in
   let s = really_input_string ic (in_channel_length ic) in
   let swagger = Swagger_j.swagger_of_string s in
-  let signature =
-    Gen.of_swagger
-      ~path_base:"/api/"
-      ~definition_base:"io.k8s."
-      ~reference_base:"#/definitions/io.k8s."
-      swagger in
-  printf "%s\n%!" (signature |> Gen.to_string)
+  Gen.of_swagger
+    ~path_base:"/api/"
+    ~definition_base:"io.k8s."
+    ~reference_base:"#/definitions/io.k8s."
+    swagger
+  |> Gen.to_string
+  |> printf "%s\n%!"
