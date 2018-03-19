21adbd534fcc973989564991dfcbd9732a6a6f9c
Andre Nathan <andre@digirati.com.br>
2018-03-19 14:36:06 -0300

    Turn code into a library

--- src/swagger.ml
+++ src/swagger.ml
@@ -1,14 +1,19 @@
 open Printf
 
-let () =
-  let ic = open_in "src/swagger.json" in
+let codegen ~path_base
+            ~definition_base
+            ~reference_base
+            ~reference_root
+            ?(output = stdout)
+            ~input =
+  let ic = open_in input in
   let s = really_input_string ic (in_channel_length ic) in
   let swagger = Swagger_j.swagger_of_string s in
   Gen.of_swagger
-    ~path_base:"/"
-    ~definition_base:"io.k8s."
-    ~reference_base:"#/definitions/io.k8s."
-    ~reference_root:"Definitions"
+    ~path_base
+    ~definition_base
+    ~reference_base
+    ~reference_root
     swagger
   |> Gen.to_string
-  |> printf "%s\n%!"
+  |> fprintf output "%s\n%!"
