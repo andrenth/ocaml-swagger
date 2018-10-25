d463091f6366ed5c7078166bc40a5564ba7308c6
Richard Davison <rnd1@solvuu.com>
2018-10-25 18:16:08 -0400

    Add adapter to extract vendor extensions from swagger.json

--- src/swagger_util.ml
+++ src/swagger_util.ml
@@ -9,3 +9,30 @@ module Validate = struct
     let len = String.length s in
     len > min && len < max
 end
+
+module Adapters = struct
+  module Extensions : Atdgen_runtime.Json_adapter.S = struct
+    (* val normalize : Yojson.Safe.json -> Yojson.Safe.json *)
+    let normalize json =
+      Yojson.Safe.Util.to_assoc json
+      |> List.partition (fun (name, _) ->
+           (String.length name >= 2) && (name.[0] = 'x' && name.[1] = '-'))
+      |> (fun (extensions, rest) ->
+           `Assoc (("extensions", `Assoc extensions) :: rest))
+
+    (* val restore : Yojson.Safe.json -> Yojson.Safe.json *)
+    let restore json =
+      Yojson.Safe.Util.to_assoc json
+      |> List.partition (fun (name, _) ->
+           String.equal name "extensions")
+      |> (fun (extensions, rest) ->
+           match extensions with
+           | [] -> `Assoc rest
+           | [(_, x)] ->
+             let assoc = Yojson.Safe.Util.to_assoc x in
+             `Assoc (assoc @ rest)
+           | (_ : (string * Yojson.Safe.json) list) ->
+             failwith "invalid json: assoc list contains duplicates")
+      in
+  end
+end
